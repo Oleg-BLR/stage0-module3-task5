3b914a5f7763226a2549d4aa1062bc27b41d2a05
Oleg <Oleg-BLR@yandex.ru>
2022-10-20 20:17:51 +0300

    solution 01

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+
+    public static void main(String[] args) {
+        divide(9);
+    }
+    static void divide(double toBeDivided) {
+        double devidedByFiveNumber;
+        System.out.println(toBeDivided + " devided by 5 equals " + (devidedByFiveNumber = toBeDivided/5));
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public void convert(float minutes) {
+
+    public static void main(String[] args) {
+
+        convert(2);
+    }
+
+    private static void convert(float minutes) {
+        float seconds;
+        System.out.println(minutes + " minutes equals " + (seconds = minutes * 60) + " seconds");
+
     }
 }
